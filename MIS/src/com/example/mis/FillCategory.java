f235a039530ce9ee4825d5734cf8cb5779b713bd
MobileInformationSystem <mr.matthias.mueller@gmail.com>
2014-05-21 13:53:58 +0200

    Utility classes for data-connection and communication

--- MIS/src/com/example/mis/FillCategory.java
+++ MIS/src/com/example/mis/FillCategory.java
@@ -3,33 +3,72 @@ package com.example.mis;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Vector;
 
 public class FillCategory {
 
 	private DB_Connect connection;
 	private ResultSet db_data;
+	private String category;
+	private Vector<Point> points;
 
-	public FillCategory(String category) {
+	public FillCategory() {
+		points = null;
+	}
+	
+	public FillCategory(String param_category) {
+		points = null;
+        setCategory(param_category);
+	}
+	
+	public void getDataFromDB()
+	{
 		connection = new DB_Connect();
 		String sql = "SELECT * FROM MIS WHERE category='" + category + "';";
 		db_data = connection.readDatabase(sql);
 		connection.close();
 	}
 
-	/**
-	public List<Point> getPoints() {
-		List<Point> points = null;
-		try {
-			while (db_data.next()) {
-				points.add(new Point());
-
-			}
-		} catch (SQLException ex) {
+	public void getData()
+	{
+		getDataFromDB();
+		
+		String db_name;
+		int db_id;
+		String db_category;
+		int db_rating_num;
+		int db_rating_sum;
+		String db_map;
+		
+		Vector<Point> data_points = null;
+	try {
+		while (db_data.next()) {
+			db_name = db_data.getString("Name");
+			db_id = db_data.getInt("ID");
+			db_category = db_data.getString("Category");
+			db_rating_num = db_data.getInt("Rating_Num");
+			db_rating_sum = db_data.getInt("Rating_Sum");
+			db_map = db_data.getString("Map");
+			
+			data_points.add(new Point(db_id, db_name, db_category, db_rating_num, db_rating_sum, db_map));
 		}
-
-		return points;
-
+	} catch (SQLException e) {
+	    }
+    points = data_points;
 	}
-	**/
+	
+	public void setCategory(String param_category)
+	{
+		points.removeAllElements();
+		category = param_category;
+		getData();
+		
+	}
+	
+	public Vector<Point> getPoints()
+	{
+		return points;
+	}
+	
 
 }
--- MIS/src/com/example/mis/Point.java
+++ MIS/src/com/example/mis/Point.java
@@ -6,12 +6,16 @@ public class Point {
 	private String adress;
 	private int id;
 	private String category;
+	private int rating_num;
+	private int rating_sum;
 
-	public Point(String param_name, String param_adress, String param_category, int param_id) {
+	public Point(int param_id, String param_name,  String param_category, int param_rating_num, int param_rating_sum, String param_adress) {
 		name = param_name;
 		adress = param_adress;
 		id = param_id;
 		category = param_category;
+		rating_num = param_rating_num;
+		rating_sum = param_rating_sum;
 	}
 	
 	
@@ -35,5 +39,13 @@ public class Point {
 	{
 		return id;
 	}
+	public int getRatingSum()
+	{
+		return rating_sum;
+	}
+	public int getRatingNum()
+	{
+		return rating_num;
+	}	
 
 }
